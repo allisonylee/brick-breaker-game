e0e4ba3fc162962cc721454a634d2520a4e10655
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-12 08:07:21 -0500

    brick-09: Add keyboard-controlled paddle

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Ball from "./model/ball.js";
+import Paddle from "./model/paddle.js";
 import "./style.css";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 const ball = new Ball(canvas.width/2, canvas.height-30, 10, 10, "#0095DD", 2, -2);
+const paddle = new Paddle((canvas.width-75)/2, canvas.height-10, 75, 10, "#0095DD");
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ball.draw(ctx);
     ball.move();
     ball.bounce(canvas.width, canvas.height);
+    paddle.draw(ctx);
+    paddle.move();
 
     window.requestAnimationFrame(draw);
 }
