c7b69e00f24954d5116e19248c3c505ad5245a33
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-12 08:00:29 -0500

    brick-07: Add Ball class with edge bouncing

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import Block from "./model/block.js";
-import Sprite from "./model/sprite.js";
+import Ball from "./model/ball.js";
 import "./style.css";
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-const redBlock = new Block(20, 40, 50, 50, "##FF0000");
-
-const blueSprite = new Sprite(canvas.width/2, canvas.height-30, 10, 10, "#0095DD", 2, -2);
+const ball = new Ball(canvas.width/2, canvas.height-30, 10, 10, "#0095DD", 2, -2);
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    redBlock.draw(ctx);
-    blueSprite.draw(ctx);
-    blueSprite.move();
+    ball.draw(ctx);
+    ball.move();
+    ball.bounce(canvas.width, canvas.height);
 
     window.requestAnimationFrame(draw);
 }
