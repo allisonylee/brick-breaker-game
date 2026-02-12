615825e8a8b3d70cc62d105ffe53ecf419ef24e0
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-12 08:19:16 -0500

    brick-14: Create brick grid and integrate into game loop

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Ball from "./model/ball.js";
 import Paddle from "./model/paddle.js";
+import Brick from "./model/brick.js";
 import "./style.css";
 
 const canvas = document.getElementById("myCanvas");
@@ -10,8 +11,30 @@ const paddle = new Paddle((canvas.width-75)/2, canvas.height-10, 75, 10, "#0095D
 
 let isGameOver = false;
 
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const bricks = [];
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+        let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks.push(new Brick(brickX, brickY, brickWidth, brickHeight, "#0095DD"));
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    bricks.forEach((brick) => {
+        brick.draw(ctx);
+        brick.collides(ball);
+    })
     
     ball.draw(ctx);
     ball.move();
