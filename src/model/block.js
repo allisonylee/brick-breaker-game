e6b1adc873c5dc90cd392344c8589a05307befa9
allisonylee <88457470+allisonylee@users.noreply.github.com>
2026-02-12 07:41:42 -0500

    brick-03: Add Block class with constructor and draw method

---
+++ src/model/block.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.color = color;
+    }
+}
